e80d6e37f51a1469486d6a31d70264c05c2ce761
tsigo <tsigo@sloth.local>
2008-12-11 15:48:41 -0500

    Raids controller, with Raids#index

--- app/controllers/raids_controller.rb
+++ app/controllers/raids_controller.rb
@@ -1,2 +1,11 @@
 class RaidsController < ApplicationController
+  layout 'poison'
+  
+  def index
+    @raids = Raid.find(:all, :order => "date DESC")
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
 end
