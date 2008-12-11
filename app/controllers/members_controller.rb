576676088ef3e7795a2809fb9f00753ae1f49303
tsigo <tsigo@sloth.local>
2008-12-11 15:19:45 -0500

    Add Members#index action

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,2 +1,9 @@
 class MembersController < ApplicationController
+  def index
+    @members = Member.find(:all, :order => "lf DESC, name ASC")
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
 end
