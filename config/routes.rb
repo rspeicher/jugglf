bc1cc860f346b96a0df8bfecd6bc2f467f6990dc
tsigo <tsigo@sloth.local>
2008-12-16 18:24:11 -0500

    Add default route.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ ActionController::Routing::Routes.draw do |map|
 
   map.connect ':controller/:action/:id'
   map.connect ':controller/:action/:id.:format'
+  
+  map.root :controller => "members"
 end
