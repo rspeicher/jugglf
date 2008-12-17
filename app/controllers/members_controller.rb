c4c772f0c04abd951e6f22b7c00b1c055c33321b
tsigo <tsigo@sloth.local>
2008-12-16 19:03:13 -0500

    Respond to Member#new and Member#edit actions

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -17,4 +17,50 @@ class MembersController < ApplicationController
       wants.html
     end
   end
+  
+  def new
+    @member = Member.new
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def edit
+    @member = Member.find(params[:id])
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def create
+    @member = Member.new(params[:member])
+  
+    respond_to do |format|
+      if @member.save
+        flash[:notice] = 'Member was successfully created.'
+        format.html { redirect_to(@member) }
+        format.xml  { render :xml => @member, :status => :created, :location => @member }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @member.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
+  
+  def update
+    @member = Member.find(params[:id])
+
+    respond_to do |format|
+      if @member.update_attributes(params[:member])
+        flash[:notice] = 'Member was successfully updated.'
+        format.html { redirect_to(@member) }
+        format.xml  { head :ok }
+      else
+        format.html { render :action => "edit" }
+        format.xml  { render :xml => @member.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,4 @@
 class Member < ActiveRecord::Base
   has_many :attendees
-  has_many :raids, :through => :attendees
+  has_many :raids, :through => :attendees  
 end
