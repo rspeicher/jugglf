ef73df77573b8d0334d9e64a9d47246782243d5b
rspeicher <rspeicher@gmail.com>
2009-01-12 05:13:37 -0500

    Add 'Raid.is_in_last_thirty_days?' (and ninety) methods with tests

--- app/models/raid.rb
+++ app/models/raid.rb
@@ -16,6 +16,13 @@ class Raid < ActiveRecord::Base
   has_many :items
   has_many :members, :through => :attendees
   
+  def is_in_last_thirty_days?
+    self.date >= 30.days.ago.to_datetime
+  end
+  def is_in_last_ninety_days?
+    self.date >= 90.days.ago.to_datetime
+  end
+  
   def self.count_last_thirty_days
     Raid.count(:conditions => [ "date >= ?", 30.days.ago ])
   end
--- test/unit/raid_test.rb
+++ test/unit/raid_test.rb
@@ -1,6 +1,22 @@
 require 'test_helper'
 
 class RaidTest < ActiveSupport::TestCase
+  test "raid is in last <x> days methods" do
+    r = raids(:yesterday)
+    assert_equal(true, r.is_in_last_thirty_days?)
+    assert_equal(true, r.is_in_last_ninety_days?)
+    
+    r = raids(:two_months_ago)
+    assert_equal(false, r.is_in_last_thirty_days?)
+    assert_equal(true, r.is_in_last_ninety_days?)
+  end
+  
+  test "day counts return zero with no records" do
+    Raid.destroy_all
+    
+    assert_equal(0, Raid.count_last_ninety_days)
+  end
+  
   test "count last thirty days" do
     assert_equal(2, Raid.count_last_thirty_days)
   end
