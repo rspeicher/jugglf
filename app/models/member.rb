aa0227c0385e75402231a8a108ec56ccc7e7020b
rspeicher <rspeicher@gmail.com>
2009-01-12 05:17:00 -0500

    Fill out the Member.update_cache callback; passes all tests, but I'm sure it's overly resource intensive

--- app/models/member.rb
+++ app/models/member.rb
@@ -24,7 +24,9 @@
 class Member < ActiveRecord::Base
   has_many :attendees
   has_many :items
-  has_many :raids, :through => :attendees  
+  has_many :raids, :through => :attendees
+  
+  alias_method :attendance, :attendees
   
   before_save :update_cache
   after_update :increment_uncached_updates
@@ -40,5 +42,52 @@ class Member < ActiveRecord::Base
     
     def update_cache
       return unless self.should_recache?
+      
+      # Total possible attendance totals
+      totals = {
+        :thirty   => Raid.count_last_thirty_days * 1.00,
+        :ninety   => Raid.count_last_ninety_days * 1.00,
+        :lifetime => Raid.count * 1.00
+      }
+      
+      # My attendance totals
+      att = { :thirty => 0.00, :ninety => 0.00, :lifetime => 0.00 }
+      self.attendance.each do |a|
+        if totals[:thirty] > 0.00 and a.raid.is_in_last_thirty_days?
+          att[:thirty] += a.attendance
+        end
+        
+        if totals[:ninety] > 0.00 and a.raid.is_in_last_ninety_days?
+          att[:ninety] += a.attendance
+        end
+        
+        if totals[:lifetime] > 0.00
+          att[:lifetime] += a.attendance
+        end
+      end
+      
+      self.attendance_30       = (att[:thirty]   / totals[:thirty])   if totals[:thirty]
+      self.attendance_90       = (att[:ninety]   / totals[:ninety])   if totals[:ninety]
+      self.attendance_lifetime = (att[:lifetime] / totals[:lifetime]) if totals[:lifetime]
+      
+      # Loot Factor
+      lf = { :lf => 0.00, :sitlf => 0.00, :bislf => 0.00 }
+      self.items.each do |i|
+        if i.affects_loot_factor?
+          if i.situational?
+            lf[:sitlf] += i.price
+          elsif i.best_in_slot?
+            lf[:bislf] += i.price
+          else
+            lf[:lf] += i.price
+          end
+        end
+      end
+      
+      unless self.attendance_30 == 0.0
+        self.lf    = (lf[:lf]    / self.attendance_30)
+        self.sitlf = (lf[:sitlf] / self.attendance_30)
+        self.bislf = (lf[:bislf] / self.attendance_30)
+      end
     end
 end
--- test/unit/member_test.rb
+++ test/unit/member_test.rb
@@ -1,6 +1,8 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
+  fixtures :raids
+  
   test "should not recache based on updated_at" do
     m = members(:tsigo)
     
@@ -18,5 +20,55 @@ class MemberTest < ActiveSupport::TestCase
     m.uncached_updates = 2
     
     assert_equal(true, m.should_recache?)
+  end
+  
+  test "has raid attendance" do
+    m = members(:tsigo)
+    m.raids << raids(:today)
+    m.raids << raids(:yesterday)
+    
+    assert_equal(2, m.attendance.count)
+  end
+  
+  test "has item purchases" do
+    m = members(:tsigo)
+    assert_equal(0, m.items.count)
+    
+    i = Item.create(:name => 'Warglaive of Azzinoth', :price => 5.00)
+    m.items << i
+    
+    assert_equal(1, m.items.count)
+    assert_equal(m.id, i.buyer.id)
+  end
+  
+  test "cache gets updated" do
+    m = members(:update_cache)
+    assert_equal(true, m.should_recache?)
+    
+    # Add raid attendance
+    m.attendance << Attendee.create(:raid_id => raids(:today).id, :attendance => 1.00)
+    m.attendance << Attendee.create(:raid_id => raids(:yesterday).id, :attendance => 1.00)
+    # m.attendance << Attendee.create(:raid_id => raids(:two_months_ago).id, :attendance => 1.00)
+    
+    # Add an item
+    m.items << Item.create(:name => 'Normal LF', :price => 5.00, 
+      :raid_id => raids(:yesterday).id)
+    m.items << Item.create(:name => 'Sit LF', :price => 30.00, 
+      :raid_id => raids(:today).id, :situational => true)
+    m.items << Item.create(:name => 'BiS LF, Not Affected', :price => 10.00, 
+      :raid_id => raids(:two_months_ago).id, :best_in_slot => true)
+    m.items << Item.create(:name => 'BiS LF', :price => 3.14, 
+      :raid_id => raids(:yesterday).id, :best_in_slot => true)
+    assert_equal(4, m.items.count)
+    
+    m.save!
+    
+    m = Member.find_by_name('UpdateMyCache')
+    assert_equal(1.00, m.attendance_30)
+    assert_equal(0.666667, m.attendance_90)
+    
+    assert_equal(5.00, m.lf)
+    assert_equal(30.00, m.sitlf)
+    assert_equal(3.14, m.bislf)
   end
 end
