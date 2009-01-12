0cf46aa61b918052b55f1784176c8fe2d362238c
rspeicher <rspeicher@gmail.com>
2009-01-12 03:07:00 -0500

    Change member_attendee? helper to member_attendance since it doesn't return bool

--- app/helpers/members_helper.rb
+++ app/helpers/members_helper.rb
@@ -1,6 +1,6 @@
 module MembersHelper
   # FIXME: This is ugly and potentially resource intensive
-  def member_attendee?(raid, member)
+  def member_attendance(raid, member)
     if raid.members.include? member
       return raid_attendance_colored(raid.attendees.find_by_member_id(@member).attendance)
     else
