0aff8c1bbc4e8b9dce271b4c30ef836531c67544
rspeicher <rspeicher@gmail.com>
2009-01-12 22:35:14 -0500

    Delete the unique name index on Members and instead rely on validates_uniqueness_of

--- db/migrate/20081216233039_add_unique_name_index_to_members.rb
+++
@@ -1,9 +0,0 @@
-class AddUniqueNameIndexToMembers < ActiveRecord::Migration
-  def self.up
-    add_index :members, [:name], :unique => true
-  end
-
-  def self.down
-    remove_index :members, :column => [:name]
-  end
-end
