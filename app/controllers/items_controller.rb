bf01a202a9db6f48f736988746a236a5ed400bee
rspeicher <rspeicher@gmail.com>
2009-01-12 23:07:23 -0500

    Add Item#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,4 +8,14 @@ class ItemsController < ApplicationController
       wants.html
     end
   end
+  
+  def show
+    @item = Item.find(params[:id])
+    # @purchases = Item.find_all_by_name(@item.name)
+    @purchases = Item.find(:all, :include => :raid, :conditions => ["name = ?", @item.name]) 
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
 end
