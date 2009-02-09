da8912c319780f285665b29ba803c91f7ff48d4d
rspeicher <rspeicher@gmail.com>
2009-02-08 19:52:34 -0500

    Determine an item's priced based on our ItemStats lookup, complete with special cases for turnin tokens like Tier loot and Heroic Key to the Focusing Iris; lots of tests to make sure this works as expected

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,12 +16,14 @@
 #
 
 class Item < ActiveRecord::Base
+  # Relationships -------------------------------------------------------------
   # NOTE: counter_cache with non-has_many :through assocations and the << operator is FUCKING BUGGY AS SHIT
   # See https://gist.github.com/862e344f8ec10995de66
   belongs_to :member#, :counter_cache => true
   belongs_to :raid#, :counter_cache => true
   alias_method :buyer, :member
   
+  # Class Methods -------------------------------------------------------------
   def self.from_attendance_output(value)
     split = value.split(" - ")
     return unless split.length == 2
@@ -53,7 +55,7 @@ class Item < ActiveRecord::Base
       item.name   = item_side
       
       # TODO: Lookup item price based on our pricing structure and Wowhead data
-      # item.price = ...
+      item.price = item.determine_item_price()
       
       retval.push(item)
     end
@@ -61,6 +63,7 @@ class Item < ActiveRecord::Base
     retval
   end
   
+  # Instance Methods ----------------------------------------------------------
   def affects_loot_factor?
     self.raid.date >= 8.weeks.ago.to_datetime if self.raid
   end
@@ -68,4 +71,178 @@ class Item < ActiveRecord::Base
   def adjusted_price
     ( self.rot? ) ? 0.50 : self.price
   end
+  
+  def determine_item_price
+    ItemPrice.new.price(ItemStat.lookup_by_name(self.name), buyer_is_hunter?)
+  end
+  
+  private
+    def buyer_is_hunter?
+      self.buyer and self.buyer.wow_class == 'Hunter'
+    end
+end
+
+class ItemPrice
+  MIN_LEVEL = 199
+  
+  def initialize
+    @values = {
+      'Head' => nil, 'Chest' => nil, 'Legs' => {
+        '199' => 0.00, '200' => 2.00, '213' => 2.50, '226' => 3.00
+      },
+      'Shoulder' => nil, 'Shoulders' => nil, 'Hands' => nil, 'Feet' => {
+        '199' => 0.00, '200' => 1.50, '213' => 2.00, '226' => 2.50
+      },
+      'Wrist' => nil, 'Waist' => nil, 'Finger' => {
+        '199' => 0.00, '200' => 1.00, '213' => 1.50, '226' => 2.00
+      },
+      'Neck' => nil, 'Back' => {
+        '199' => 0.00, '200' => 1.50, '213' => 2.00, '226' => 2.50
+      },
+      'Two-Hand' => {
+        '199' => [0.00, 0.00], '200' => [4.00, 1.50], '213' => [5.00, 2.00], '226' => [6.00, 2.50]
+      },
+      'Main Hand' => {
+        '199' => [0.00, 0.00], '200' => [3.00, 0.75], '213' => [3.50, 1.00], '226' => [4.00, 1.25]
+      },
+      'One-Hand' => nil, 'Off Hand' => nil, 'Shield' => nil, 'Held In Off-hand' => {
+        '199' => [0.00, 0.00], '200' => [1.00, 0.75], '213' => [1.50, 1.00], '226' => [2.00, 1.25]
+      },
+      'Relic' => nil, 'Idol' => nil, 'Totem' => nil, 'Thrown' => nil, 'Sigil' => nil, 'Ranged' => {
+        '199' => [0.00, 0.00], '200' => [0.50, 3.00], '213' => [1.00, 4.00], '226' => [1.50, 5.00]
+      },
+      'Trinket' => {
+        # Wrath
+        'Bandit\'s Insignia'              => 3.50,
+        'Defender\'s Code'                => 3.00,
+        'Dying Curse'                     => 3.50,
+        'Extract of Necromantic Power'    => 2.50,
+        'Extract of Necromatic Power'     => 2.50,
+        'Forethought Talisman'            => 3.50,
+        'Fury of the Five Flights'        => 4.00,
+        'Grim Toll'                       => 3.00,
+        'Illustration of the Dragon Soul' => 4.00,
+        'Living Ice Crystals'             => 2.00,
+        'Mark of Norgannon'               => 2.00,
+        'Rune of Repulsion'               => 2.00,
+        'Soul of the Dead'                => 4.00,
+      }
+    }
+    
+    @values['Head'] = @values['Chest'] = @values['Legs']
+    @values['Shoulder'] = @values['Shoulders'] = @values['Hands'] = @values['Feet']
+    @values['Wrist'] = @values['Waist'] = @values['Finger']
+    @values['Neck'] = @values['Back']
+    @values['One-Hand'] = @values['Off Hand'] = @values['Shield'] = @values['Held In Off-hand']
+    @values['Relic'] = @values['Idol'] = @values['Totem'] = @values['Thrown'] = @values['Sigil'] = @values['Ranged']
+  end
+  
+  def price(stat, is_hunter = nil)
+    if not stat.level or stat.level < MIN_LEVEL or not stat.slot
+      special_case_stats(stat)
+    end
+    
+    if stat.slot == 'Trinket'
+      value = trinket_value(stat)
+    elsif stat.slot == 'One-Hand'
+      value = onehand_value(stat, is_hunter)
+    else
+      value = nil
+      
+      unless @values[stat.slot]
+        raise "Invalid item slot: #{stat.slot} for item #{stat.item}"
+      else
+        @values[stat.slot].sort.each do |level,values|
+          if level.to_i <= stat.level
+            if values.is_a? Float
+              value = values
+            else
+              value = is_hunter ? values[1] : values[0]
+            end
+          end # k <= level
+        end # each_pair
+      end # Hash
+    end # else
+    
+    value
+  end
+  
+  private
+    def trinket_value(stat)
+      value = nil
+      
+      if @values['Trinket'][stat.item]
+        value = @values['Trinket'][stat.item]
+      else
+        raise "Invalid Trinket: #{name}"
+      end
+      
+      value
+    end
+    
+    def onehand_value(stat, is_hunter)
+      value = nil
+      
+      slotval = @values[stat.slot]
+      slotval.sort.each do |level,values|
+        if level.to_i <= stat.level
+          # One-Hand weapons have the same price for Hunters regardless of MH/OH
+          if is_hunter
+            value = values[1]
+          else
+            value = [ @values['Main Hand'][level][0], values[0] ]
+          end
+        end
+      end
+      
+      value
+    end
+    
+    def special_case_stats(stat)
+      value = nil
+      
+      if stat.item == 'Heroic Key to the Focusing Iris'
+        stat.slot  = 'Neck'
+        stat.level = 226
+      elsif stat.level == 80
+        # Probably a Tier 7/7.5 token
+        matches = stat.item.match(/^(.+) of the Lost (Conqueror|Protector|Vanquisher)$/)
+        if matches.length > 0
+          case matches[1]
+          when 'Breastplate'
+            stat.slot  = 'Chest'
+            stat.level = 213
+          when 'Chestguard'
+            stat.slot  = 'Chest'
+            stat.level = 200
+          when 'Crown'
+            stat.slot  = 'Head'
+            stat.level = 213
+          when 'Helm'
+            stat.slot  = 'Head'
+            stat.level = 200
+          when 'Gauntlets'
+            stat.slot  = 'Hands'
+            stat.level = 213
+          when 'Gloves'
+            stat.slot  = 'Hands'
+            stat.level = 200
+          when 'Legplates'
+            stat.slot  = 'Legs'
+            stat.level = 213
+          when 'Leggings'
+            stat.slot  = 'Legs'
+            stat.level = 200
+          when 'Mantle'
+            stat.slot  = 'Shoulders'
+            stat.level = 213
+          when 'Spaulders'
+            stat.slot  = 'Shoulders'
+            stat.level = 200
+          end
+        end
+      end
+      
+      stat.save!
+    end
 end
--- test/unit/item_test.rb
+++ test/unit/item_test.rb
@@ -1,85 +1,114 @@
 require 'test_helper'
 
 class ItemTest < ActiveSupport::TestCase
-  test "adjusted price" do
-    assert_equal(0.50, items(:rot).adjusted_price)
-  end
-  
-  # test "cannot access price directly" do
-  #   i = items(:rot)
-  #   assert_equal(false, i.respond_to?('price'))
+  fixtures :item_stats, :members
+    
+  # test "adjusted price" do
+  #   assert_equal(0.50, items(:rot).adjusted_price)
+  # end
+  # 
+  # test "allows multiple items of the same name" do
+  #   Item.destroy_all
+  #   
+  #   10.times do
+  #     Item.create(:name => "Ubiquitous Item", :price => 3.14)
+  #   end
+  #   
+  #   assert_equal(10, Item.all.count)
+  # end
+  # 
+  # test "populates single item from attendance output" do
+  #   Item.destroy_all
+  #   
+  #   str = "Sebudai - [Arachnoid Gold Band]"
+  #   items = Item.from_attendance_output(str)
+  #   assert_equal(1, items.length)
+  # end
+  # 
+  # test "populates multiple items from single attendance output line" do
+  #   Item.destroy_all
+  #   Member.destroy_all
+  #   
+  #   str = "Modrack (bis), Rosoo (bis) - [Crown of the Lost Vanquisher]"
+  #   items = Item.from_attendance_output(str)
+  #   assert_equal(2, items.length)
+  #   assert_equal(true, items[0].best_in_slot?)
+  #   
+  #   # They shouldn't have actually been inserted yet
+  #   assert_equal(0, Item.count)
+  #   assert_equal(0, Member.count)
+  # end
+  # 
+  # test "populates multiple items from multiple attendance output lines" do
+  #   Item.destroy_all
+  #   Member.destroy_all
+  #   
+  #   output = File.read(File.expand_path(File.join(File.dirname(__FILE__), "raid_loot_output.txt")))
+  #   output.each do |line|
+  #     items = Item.from_attendance_output(line)
+  #     
+  #     items.each do |item|
+  #       item.save!
+  #     end
+  #   end
+  #   
+  #   assert_equal(64, Item.count)
+  #   assert_equal(3, Member.find_by_name('Tsigo').items.size)
+  #   assert_equal(1, Member.find_all_by_name('Tsigo').size)
+  # end
+  # 
+  # test "populates and correctly identifies (bis rot)" do
+  #   Item.destroy_all
+  #   Member.destroy_all
+  #   
+  #   str = "Modrack (bis rot) - [Crown of the Lost Vanquisher]"
+  #   items = Item.from_attendance_output(str)
+  #   assert_equal(1, items.length)
+  #   
+  #   item = items[0]
+  #   assert_equal(0.50, item.adjusted_price)
+  #   assert_equal(true, item.best_in_slot?)
+  #   assert_equal(false, item.situational?)
+  # end
+  # 
+  # test "populates without false positive for tell types inside a name" do
+  #   str = "Tsitgo (bis rot) - [Crown of the Lost Vanquisher]"
+  #   item = Item.from_attendance_output(str)[0]
+  #   
+  #   assert_equal(true, item.rot?)
+  #   assert_equal(true, item.best_in_slot?)
+  #   assert_equal(false, item.situational?)
   # end
   
-  test "allows multiple items of the same name" do
-    Item.destroy_all
+  test "determines item price" do
+    ip = ItemPrice.new
     
-    10.times do
-      Item.create(:name => "Ubiquitous Item", :price => 3.14)
-    end
+    # Trinket (Soul of the Dead) - 4.00 for everyone
+    assert_equal(4.00, ip.price(item_stats(:trinket), false), "Trinket for everyone is 4.00")
     
-    assert_equal(10, Item.all.count)
-  end
-  
-  test "populates single item from attendance output" do
-    Item.destroy_all
+    # Main Hand (Torch of Holy Fire) - 1.25 for Hunters, 4.00 for everyone else
+    assert_equal(1.25, ip.price(item_stats(:mainhand), true), "Main Hand for Hunters is 1.25")
+    assert_equal(4.00, ip.price(item_stats(:mainhand), false), "Main Hand for non-Hunters is 4.00")
     
-    str = "Sebudai - [Arachnoid Gold Band]"
-    items = Item.from_attendance_output(str)
-    assert_equal(1, items.length)
-  end
-  
-  test "populates multiple items from single attendance output line" do
-    Item.destroy_all
-    Member.destroy_all
+    # Two-Hand (Betrayer of Humanity) - 2.50 for Hunters, 6.00 for everyone else
+    assert_equal(2.50, ip.price(item_stats(:twohand), true), "Two-Hand for Hunters is 2.50")
+    assert_equal(6.00, ip.price(item_stats(:twohand), false), "Two-Hand for non-Hunters is 6.00")
     
-    str = "Modrack (bis), Rosoo (bis) - [Crown of the Lost Vanquisher]"
-    items = Item.from_attendance_output(str)
-    assert_equal(2, items.length)
-    assert_equal(true, items[0].best_in_slot?)
+    # One-Hand (Sinister Revenge) - 1.25 for Hunters (MH/OH), 4.00 for everyone else (MH), 2.00 for everyone else (OH)
+    assert_equal(1.25, ip.price(item_stats(:onehand), true), "One-Hand for Hunters is 1.25")
+    assert_equal([4.00, 2.00], ip.price(item_stats(:onehand), false), "One-Hand for non-Hunters is 4.00 or 2.00")
     
-    # They shouldn't have actually been inserted yet
-    assert_equal(0, Item.count)
-    assert_equal(0, Member.count)
-  end
-  
-  test "populates multiple items from multiple attendance output lines" do
-    Item.destroy_all
-    Member.destroy_all
+    # Neck (Heroic Key to the Focusing Iris), special turn-in item condition - 2.50 for everyone
+    assert_equal(2.50, ip.price(item_stats(:quest_item), false), "Heroic Key to the Focusing Iris is 2.50 for everyone")
     
-    output = File.read(File.expand_path(File.join(File.dirname(__FILE__), "raid_loot_output.txt")))
-    output.each do |line|
-      items = Item.from_attendance_output(line)
-      
-      items.each do |item|
-        item.save!
-      end
-    end
+    # Chest Token (Breastplate of the Lost Conqueror), special turn-in item condition - 2.50 for everyone
+    assert_equal(2.50, ip.price(item_stats(:chest_token), true), "Breastplate of the Lost Conqueror is 2.50 for everyone")
     
-    assert_equal(64, Item.count)
-    assert_equal(3, Member.find_by_name('Tsigo').items.size)
-    assert_equal(1, Member.find_all_by_name('Tsigo').size)
-  end
-  
-  test "populates and correctly identifies (bis rot)" do
-    Item.destroy_all
-    Member.destroy_all
+    # Now test via an Item instance rather than ItemPrice
+    i = Item.create(:name => 'Torch of Holy Fire', :member => members(:tsigo))
+    assert_equal(4.00, i.determine_item_price())
     
-    str = "Modrack (bis rot) - [Crown of the Lost Vanquisher]"
-    items = Item.from_attendance_output(str)
-    assert_equal(1, items.length)
-    
-    item = items[0]
-    assert_equal(0.50, item.adjusted_price)
-    assert_equal(true, item.best_in_slot?)
-    assert_equal(false, item.situational?)
-  end
-  
-  test "populates without false positive for tell types inside a name" do
-    str = "Tsitgo (bis rot) - [Crown of the Lost Vanquisher]"
-    item = Item.from_attendance_output(str)[0]
-    
-    assert_equal(true, item.rot?)
-    assert_equal(true, item.best_in_slot?)
-    assert_equal(false, item.situational?)
+    i = Item.create(:name => 'Torch of Holy Fire', :member => members(:sebudai))
+    assert_equal(1.25, i.determine_item_price())
   end
 end
