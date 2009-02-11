3b755cb78e1151353ee1b1dcfad8507393f6a2a0
rspeicher <rspeicher@gmail.com>
2009-02-11 00:23:08 -0500

    Add date to Raid's valid_attributes

--- spec/models/raid_spec.rb
+++ spec/models/raid_spec.rb
@@ -3,6 +3,7 @@ require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 describe Raid do
   before(:each) do
     @valid_attributes = {
+      :date => Time.now.to_s(:db)
     }
   end
 
