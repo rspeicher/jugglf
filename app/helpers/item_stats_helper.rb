391307ecc8e6fd97956c28ae4993b9611085550b
rspeicher <rspeicher@gmail.com>
2009-02-16 17:27:10 -0500

    Make sure we have an ItemStat object before returning something

--- app/helpers/item_stats_helper.rb
+++ app/helpers/item_stats_helper.rb
@@ -2,12 +2,12 @@ module ItemStatsHelper
   def link_to_wowhead(item)
     stat = ItemStat.lookup_by_name(item.name)
     
-    "<a href=\"#{stat.wowhead_link}\" class=\"#{stat.color}\">#{h(item.name)}</a>"
+    "<a href=\"#{stat.wowhead_link}\" class=\"#{stat.color}\">#{h(item.name)}</a>" unless stat.nil?
   end
   
   def link_to_item_with_stats(item)
     stat = ItemStat.lookup_by_name(item.name)
     
-    "<a href=\"#{item_path(item)}\" rel=\"item=#{stat.item_id}\" class=\"#{stat.color}\">#{h(item.name)}</a>"
+    "<a href=\"#{item_path(item)}\" rel=\"item=#{stat.item_id}\" class=\"#{stat.color}\">#{h(item.name)}</a>" unless stat.nil?
   end
 end
