559ca3544329a171ef6a8e12c19695635e66c27f
rspeicher <rspeicher@gmail.com>
2009-02-18 06:52:11 -0500

    Reorganize helpers a bit

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,43 +6,6 @@ module ApplicationHelper
     link_to "<span>#{text}</span>", path
   end
   
-  def member_link_colored(member)
-    link_to h(member.name), member_path(member), :class => member.wow_class unless member.nil?
-  end
-  
-  def member_attendance_colored(value)
-    if value.class == Float
-      value = (value * 100).floor
-    end
-    
-    if value >= 0 and value <= 34
-      class_str = 'negative'
-    elsif value >= 35 and value <= 66
-      class_str = 'neutral'
-    elsif value >= 67 and value <= 100
-      class_str = 'positive'
-    else
-      class_str = 'neutral'
-    end
-    
-    "<span class=\"#{class_str}\">#{value}%</span>"
-  end
-  def raid_attendance_colored(value)
-    if value.class == Float
-      value = (value * 100).floor
-    end
-    
-    if value == 100
-      class_str = 'positive'
-    elsif value == 0
-      class_str = 'negative'
-    else
-      class_str = 'neutral'
-    end
-
-    "<span class=\"#{class_str}\">#{value}%</span>"
-  end
-  
   def progress_bar(width, *args)
     options = args.extract_options!
     options.symbolize_keys!
--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -7,4 +7,13 @@ module ItemsHelper
     s << ' Disenchanted' if item.buyer.nil?
     s.strip
   end
+  
+  def item_row_classes(item)
+    s = ''
+    s << ' item_bis' if item.best_in_slot?
+    s << ' item_sit' if item.situational?
+    s << ' item_rot' if item.rot?
+    s << ' item_de' if item.buyer.nil?
+    s.strip
+  end
 end
