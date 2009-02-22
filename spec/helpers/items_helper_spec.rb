a17f5d71ac36db49969078f4006222055f2747d6
rspeicher <rspeicher@gmail.com>
2009-02-22 09:16:44 -0500

    Finishing specs for helpers

---
+++ spec/helpers/items_helper_spec.rb
@@ -0,0 +1,31 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+include ItemsHelper
+
+describe ItemsHelper do
+  before(:each) do
+    Member.destroy_all
+  end
+  
+  describe "item_tell_types" do
+    it "should show comma-separated list of tell types" do
+      item = Item.make(:best_in_slot => true, :rot => true)
+      
+      item_tell_types(item).should == 'Best in Slot, Rot'
+    end
+    
+    it "should show Disenchanted for items without a buyer" do
+      item = Item.make(:member => nil)
+      
+      item_tell_types(item).should == 'Disenchanted'
+    end
+  end
+  
+  describe "item_row_classes" do
+    it "should return multiple classes" do
+      item = Item.make(:best_in_slot => true, :rot => true)
+    
+      item_row_classes(item).should == 'item_bis item_rot'
+    end
+  end
+end
