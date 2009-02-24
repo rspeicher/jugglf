c0ec3d19ba0b0958aa6b34e1b86d3387e1269e5c
rspeicher <rspeicher@gmail.com>
2009-02-23 22:21:54 -0500

    Cover ItemStatsHelper specs

--- app/helpers/item_stats_helper.rb
+++ app/helpers/item_stats_helper.rb
@@ -1,13 +1,13 @@
 module ItemStatsHelper
   def link_to_wowhead(item)
-    stat = ItemStat.lookup_by_name(item.name)
+    stat = ItemStat.lookup(item.name)
     
-    "<a href=\"#{stat.wowhead_link}\" class=\"#{stat.color}\">#{h(item.name)}</a>" unless stat.nil?
+    "<a href='#{stat.wowhead_link}' class='#{stat.color}'>#{h(item.name)}</a>" unless stat.nil?
   end
   
   def link_to_item_with_stats(item)
-    stat = ItemStat.lookup_by_name(item.name)
+    stat = ItemStat.lookup(item.name)
     
-    "<a href=\"#{item_path(item)}\" rel=\"item=#{stat.item_id}\" class=\"#{stat.color}\">#{h(item.name)}</a>" unless stat.nil?
+    "<a href='#{item_path(item)}' rel='item=#{stat.item_id}' class='#{stat.color}'>#{h(item.name)}</a>" unless stat.nil?
   end
 end
