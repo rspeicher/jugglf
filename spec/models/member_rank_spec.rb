79def13b8622526f85ab78defae540d2f2164408
rspeicher <rspeicher@gmail.com>
2009-02-23 20:00:29 -0500

    Finish up MemberRank spec

--- spec/models/member_rank_spec.rb
+++ spec/models/member_rank_spec.rb
@@ -12,57 +12,28 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe MemberRank do
-  before(:all) do
-    @valid_attributes = {
-      :name => 'Name'
-    }
+  it "should be valid" do
+    MemberRank.make.should be_valid
   end
+end
 
-  it "should create a new instance given valid attributes" do
-    MemberRank.create!(@valid_attributes)
-  end
-  
-  it "should require a name" # do
-   #    mr = MemberRank.create
-   #    mr.should_not be_valid
-   #    mr.should have(1).errors_on(:name)
-   #  end
-  
-  # ---------------------------------------------------------------------------
-  
+# -----------------------------------------------------------------------------
+
+describe MemberRank, "formatting" do
   describe "sanitization" do
-    before(:all) do
-      @mr = MemberRank.create(:name => 'Sanitization', :prefix => '<b>', 
-        :suffix => '</b>')
-      
-      @html = {
-        :xss        => '<javascript>Content</javascript>',
-        :span_style => '<span style="color: green">Content</span>',
-        :span_class => '<span class="Druid">Content</span>',
-        :opening    => '<b>',
-      }
+    before(:each) do
+      @rank = MemberRank.make(:name => 'Example')
     end
     
-    it "should format a string" # do
-     #      @mr.format('Tsigo').should == '<b>Tsigo</b>'
-     #    end
+    it "should format a string" do
+      @rank.format(@rank.name).should == '<b>Example</b>'
+    end
     
-    # it "should allow span" do
-    #   @mr.prefix = @html[:span_style]
-    #   @mr.prefix.should == @html[:span_style]
-    #   
-    #   @mr.prefix = @html[:span_class]
-    #   @mr.prefix.should == @html[:span_class]
-    # end
-    # 
-    # it "should allow no closing tag" do
-    #   @mr.prefix = @html[:opening]
-    #   @mr.prefix.should == @html[:opening]
-    # end
-    # 
-    # it "should not allow javascript" do
-    #   @mr.prefix = @html[:xss]
-    #   @mr.prefix.should_not == @html[:xss]
-    # end
+    it "should strip javascript" do
+      @rank.prefix = '<script>'
+      @rank.suffix = '</script>'
+      
+      @rank.format('Example').should == 'Example'
+    end
   end
 end
