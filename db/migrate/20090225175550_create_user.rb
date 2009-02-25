17eab904a1764771d8a260db3e5c443cec1ed4ab
rspeicher <rspeicher@gmail.com>
2009-02-25 13:10:24 -0500

    User table migration

---
+++ db/migrate/20090225175550_create_user.rb
@@ -0,0 +1,23 @@
+class CreateUser < ActiveRecord::Migration
+  def self.up
+    create_table :users do |t|
+      t.string    :login,                 :null => false
+      t.string    :crypted_password,      :null => false
+      t.string    :password_salt,         :null => false
+      t.string    :persistence_token,     :null => false
+      # t.string    :single_access_token,   :null => false # optional, see the tokens section below.
+      # t.string    :perishable_token,      :null => false # optional, see the tokens section below.
+      t.integer   :login_count,           :null => false, :default => 0
+      t.integer   :failed_login_count,    :null => false, :default => 0
+      t.datetime  :last_request_at
+      t.datetime  :current_login_at
+      t.datetime  :last_login_at
+      t.string    :current_login_ip
+      t.string    :last_login_ip
+    end
+  end
+
+  def self.down
+    drop_table :users
+  end
+end
