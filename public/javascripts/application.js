29a3b3007197ba97eb378cb35e7c8cb750285ff7
rspeicher <rspeicher@gmail.com>
2009-02-26 01:51:38 -0500

    Hide the flash success message after a few seconds.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -8,3 +8,12 @@ function sortMemberTable() {
       7: { sorter:'currency'} }, // Sit
   });
 }
+
+// Hide the flash success message after giving the user 4s to read it
+// Don't hide the error messages; we probably want to give them more time to be
+// processed by the user
+function hideSuccessFlash() {
+  setTimeout(function() {
+    $('div#flash.success').fadeOut('slow')
+  }, 4000);
+}
