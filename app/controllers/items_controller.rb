e6d398c24ebef9695e4783ca2d76a817bb6c8571
rspeicher <rspeicher@gmail.com>
2009-02-28 10:34:36 -0500

    Add Items#show

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,16 +1,16 @@
 class ItemsController < ApplicationController
-  # Show is really showing all of the purchases of a particular Item, so that should
-  # go in that controller.
-  # def show
-  #   @purchases = Item.find_all_by_item_id(:include => :raid, :order => "purchased_on DESC", :conditions => ["name = ?", @loot.name])
-  #   
-  #   respond_to do |wants|
-  #     wants.html
-  #   end
-  # end
+  before_filter :find_item, :only => [:show]
+  
+  def show
+    @loots = @item.loots.find(:all, :include => :member)
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
   
   private
-    # def find_loot
-    #   @item = Item.find(params[:id])
-    # end
+    def find_item
+      @item = Item.find(params[:id])
+    end
 end
