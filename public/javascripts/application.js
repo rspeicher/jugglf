690f55681601a194d91bda7ca25e1b1dbb23104c
rspeicher <rspeicher@gmail.com>
2009-03-02 01:45:18 -0500

    Reorganize application.js; use our zebraRows method inside toggleItemTypes to reduce some code.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,22 @@
-/* Custom sorter to sort wishlist priorities as Best in Slot > Normal > Rot > Situational */
-$.tablesorter.addParser({
-    id: 'wishlist',
-    is: function(s) {
-        return false;
-    },
-    format: function(s) { 
-        return s.toLowerCase().replace(/best in slot/,1).replace(/normal/,2).replace(/rot/,3).replace(/situational/,4);
-    },
-    type: 'numeric'
-});
+/**
+ * Re-applies alternating row colors to a specific tbody after a specified delay.
+ *
+ * tbody_id     String      ID of the tbody to apply zebra rows to
+ * delay        Integer     Delay, in miliseconds, after which to apply the effect (default: 0)
+ */
+function zebraRows(tbody_id, delay) {
+    delay = ( delay == null ) ? 0 : delay
+    
+    setTimeout(function() {
+        background = 'even';
+        $('tbody#' + tbody_id + ' > tr:visible').each(function() {
+            $(this).removeClass('even');
+            $(this).removeClass('odd');
+            $(this).addClass(background);
+            background = (background == 'even') ? 'odd' : 'even';
+        });
+    }, delay);
+}
 
 function sortMemberTable() {
     $("table#members").tablesorter({
@@ -33,26 +41,6 @@ function sortWishlistTable() {
     });
 }
 
-/**
- * Re-applies alternating row colors to a specific tbody after a specified delay.
- *
- * tbody_id     String      ID of the tbody to apply zebra rows to
- * delay        Integer     Delay, in miliseconds, after which to apply the effect (default: 0)
- */
-function zebraRows(tbody_id, delay) {
-    delay = ( delay == null ) ? 0 : delay
-    
-    setTimeout(function() {
-        background = 'even';
-        $('tbody#' + tbody_id + ' > tr:visible').each(function() {
-            $(this).removeClass('even');
-            $(this).removeClass('odd');
-            $(this).addClass(background);
-            background = (background == 'even') ? 'odd' : 'even';
-        });
-    }, delay);
-}
-
 // Hide the flash success message after giving the user 4s to read it
 // Don't hide the error messages; we probably want to give them more time to be
 // processed by the user
@@ -70,68 +58,48 @@ function hideSuccessFlash() {
     surrounding 'even' rows are shown.
 */
 function toggleItemTypes(object) {
-    if ($(object).hasClass('bis'))
-    {
-        type = 'bis'
-    }
-    else if ($(object).hasClass('sit'))
-    {
-        type = 'sit'
-    }
-    else if ($(object).hasClass('rot'))
-    {
-        type = 'rot'
-    }
-    else if ($(object).hasClass('de'))
-    {
-        type = 'de'
-    }
+    if      ($(object).hasClass('bis')) { type = 'bis' }
+    else if ($(object).hasClass('sit')) { type = 'sit' }
+    else if ($(object).hasClass('rot')) { type = 'rot' }
+    else if ($(object).hasClass('de'))  { type = 'de'  }
     
     parentRow = $(object).parent().parent();
 
     // If our parent row is .shown, then we need to toggle this filter off and show all rows
     if ($(parentRow).hasClass('shown'))
     {
-        background = 'even';
         $('#itemfilter tr').each(function() {
-            $(this).removeClass('even');
-            $(this).removeClass('odd');
-            $(this).addClass(background);
-            background = $(this).hasClass('even') ? 'odd' : 'even';
-            
             $(this).show();
             $(this).removeClass('shown');
         });
     }
     else
     {
-        background = null;
         $('#itemfilter tr').each(function() {
             if ($(this).hasClass('loot_' + type))
             {
                 // If this row has this tell type, apply 'shown' class
                 $(this).addClass('shown');
-                
-                // Zebra table backgrounds; we only need to do this if a row
-                // has already been hidden (background set to non-null below)
-                if (background != null)
-                {
-                    $(this).removeClass('even');
-                    $(this).removeClass('odd');
-                    $(this).addClass(background);
-                    background = (background == 'even' ) ? 'odd' : 'even';
-                }
             }
             else
             {
                 // Otherwise hide this row
                 $(this).hide();
-                
-                if (background == null)
-                {
-                    background = ($(this).hasClass('even')) ? 'even' : 'odd';
-                }
             }
         });
     }
+    
+    zebraRows($(parentRow).attr('id'));
 }
+
+/* Custom sorter to sort wishlist priorities as Best in Slot > Normal > Rot > Situational */
+$.tablesorter.addParser({
+    id: 'wishlist',
+    is: function(s) {
+        return false;
+    },
+    format: function(s) { 
+        return s.toLowerCase().replace(/best in slot/,1).replace(/normal/,2).replace(/rot/,3).replace(/situational/,4);
+    },
+    type: 'numeric'
+});
