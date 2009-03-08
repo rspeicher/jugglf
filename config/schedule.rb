95199eeefd423637ddd7cf6db54a78979e26a7dd
rspeicher <rspeicher@gmail.com>
2009-03-07 21:06:14 -0500

    Trying 'whenever' gem.

---
+++ config/schedule.rb
@@ -0,0 +1,28 @@
+# Use this file to easily define all of your cron jobs.
+#
+# It's helpful, but not entirely necessary to understand cron before proceeding.
+# http://en.wikipedia.org/wiki/Cron
+
+every 1.day, :at => '4:00 am' do
+  runner "Member.update_all_cache"
+end
+
+every 10.minutes do
+  runner 'Session.delete_all ["updated_at < ?", 20.minutes.ago]'
+end
+
+# Example:
+#
+# set :cron_log, "/path/to/my/cron_log.log"
+#
+# every 2.hours do
+#   command "/usr/bin/some_great_command"
+#   runner "MyModel.some_method"
+#   rake "some:great:rake:task"
+# end
+#
+# every 4.days do
+#   runner "AnotherModel.prune_old_records"
+# end
+
+# Learn more: http://github.com/javan/whenever
