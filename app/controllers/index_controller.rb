6be8d438ac5a1651ff7244736f6f3a9f6f035e83
rspeicher <rspeicher@gmail.com>
2009-03-11 08:26:02 -0400

    Fill out the Index page with some statistics. In the future we can add things like tier drop counts, Fragment of Val'anyr process, etc.

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,6 +2,18 @@ class IndexController < ApplicationController
   layout @@layout
   
   def index
+    @counts = Member.active.count(:group => 'wow_class')
+    
+    @attendance_guild = Member.active.find(:first,
+      :select => 'AVG(attendance_30) AS avg_30, AVG(attendance_90) AS avg_90, AVG(attendance_lifetime) AS avg_lifetime')
+    @attendance = Member.active.find(:all, :order => 'wow_class', :group => 'wow_class', 
+      :select => 'wow_class, AVG(attendance_30) AS avg_30, AVG(attendance_90) AS avg_90, AVG(attendance_lifetime) AS avg_lifetime')
+      
+    @lootfactor_guild = Member.active.find(:first,
+      :select => 'AVG(lf) AS avg_lf, AVG(sitlf) AS avg_sitlf, AVG(bislf) AS avg_bislf')
+    @lootfactor =   Member.active.find(:all, :order => 'wow_class', :group => 'wow_class', 
+      :select => 'wow_class, AVG(lf) AS avg_lf, AVG(sitlf) AS avg_sitlf, AVG(bislf) AS avg_bislf')
+    
     respond_to do |wants|
       wants.html
     end
