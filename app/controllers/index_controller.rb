df6d468ba33fd1e27d79b56a0f4c023071849384
rspeicher <rspeicher@gmail.com>
2009-03-11 08:48:05 -0400

    Don't include members with nil wow_class

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -2,16 +2,16 @@ class IndexController < ApplicationController
   layout @@layout
   
   def index
-    @counts = Member.active.count(:group => 'wow_class')
+    @counts = Member.active.count(:group => 'wow_class', :conditions => 'wow_class IS NOT NULL')
     
-    @attendance_guild = Member.active.find(:first,
+    @attendance_guild = Member.active.find(:first, :conditions => 'wow_class IS NOT NULL',
       :select => 'AVG(attendance_30) AS avg_30, AVG(attendance_90) AS avg_90, AVG(attendance_lifetime) AS avg_lifetime')
-    @attendance = Member.active.find(:all, :order => 'wow_class', :group => 'wow_class', 
+    @attendance = Member.active.find(:all, :conditions => 'wow_class IS NOT NULL', :order => 'wow_class', :group => 'wow_class', 
       :select => 'wow_class, AVG(attendance_30) AS avg_30, AVG(attendance_90) AS avg_90, AVG(attendance_lifetime) AS avg_lifetime')
       
-    @lootfactor_guild = Member.active.find(:first,
+    @lootfactor_guild = Member.active.find(:first, :conditions => 'wow_class IS NOT NULL',
       :select => 'AVG(lf) AS avg_lf, AVG(sitlf) AS avg_sitlf, AVG(bislf) AS avg_bislf')
-    @lootfactor =   Member.active.find(:all, :order => 'wow_class', :group => 'wow_class', 
+    @lootfactor =   Member.active.find(:all, :conditions => 'wow_class IS NOT NULL', :order => 'wow_class', :group => 'wow_class', 
       :select => 'wow_class, AVG(lf) AS avg_lf, AVG(sitlf) AS avg_sitlf, AVG(bislf) AS avg_bislf')
     
     respond_to do |wants|
