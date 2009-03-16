e83db442747c5725123bf1abb34b4478f43d8fc7
rspeicher <rspeicher@gmail.com>
2009-03-16 12:20:47 -0400

    Implement Loot #new, #edit, #create, #update, #destroy

--- app/controllers/loots_controller.rb
+++ app/controllers/loots_controller.rb
@@ -1,5 +1,6 @@
 class LootsController < ApplicationController
   before_filter :require_admin
+  before_filter :find_loot, :only => [:show, :edit, :update, :destroy]
   
   layout @@layout
   
@@ -14,4 +15,61 @@ class LootsController < ApplicationController
       wants.html
     end
   end
+  
+  def new
+    page_title('New Loot')
+    
+    @loot = Loot.new
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def edit
+    page_title(@loot.item.name, 'Edit Loot')
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def create
+    @loot = Loot.new(params[:loot])
+  
+    respond_to do |wants|
+      if @loot.save
+        flash[:success] = 'Loot was successfully created.'
+        wants.html { redirect_to(@loot) }
+      else
+        wants.html { render :action => "new" }
+      end
+    end
+  end
+  
+  def update
+    respond_to do |wants|
+      if @loot.update_attributes(params[:loot])
+        flash[:success] = 'Loot was successfully updated.'
+        wants.html { redirect_to(loot_path(@loot)) }
+      else
+        wants.html { render :action => 'edit' }
+      end
+    end
+  end
+  
+  def destroy
+    @loot.destroy
+    
+    flash[:success] = 'Loot was successfully deleted.'
+    
+    respond_to do |wants|
+      wants.html { redirect_to(loots_path) }
+    end
+  end
+  
+  private
+    def find_loot
+      @loot = Loot.find(params[:id])
+    end
 end
--- spec/controllers/loots_controller_spec.rb
+++ spec/controllers/loots_controller_spec.rb
@@ -42,3 +42,261 @@ describe LootsController, "#index" do
     end
   end
 end
+
+# -----------------------------------------------------------------------------
+# New
+# -----------------------------------------------------------------------------
+
+# GET /loots/new
+describe LootsController, "#new" do
+  def get_response
+    get :new
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      @loot = mock_model(Loot)
+      Loot.should_receive(:new).and_return(@loot)
+      get_response
+    end
+    
+    it "should assign @loot" do
+      assigns[:loot].should === @loot
+    end
+    
+    it "should render" do
+      response.should render_template(:new)
+      response.should be_success
+    end
+  end
+  
+  describe "as user" do
+    it "should not render" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Edit
+# -----------------------------------------------------------------------------
+
+# GET /loots/:id/edit
+describe LootsController, "#edit" do
+  def get_response
+    get :edit, :id => '1'
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      @item = mock_model(Item, :name => 'Name')
+      find_loot
+      @loot.should_receive(:item).and_return(@item)
+      get_response
+    end
+    
+    it "should assign @loot" do
+      assigns[:loot].should == @loot
+    end
+    
+    it "should render" do
+      response.should render_template(:edit)
+    end
+  end
+  
+  describe "as user" do
+    it "should not render" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Create
+# -----------------------------------------------------------------------------
+
+# POST /loots/create
+describe LootsController, "#create" do
+  def get_response
+    post :create, :loot => @params
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      @loot = mock_model(Loot, :to_param => '1')
+      Loot.should_receive(:new).and_return(@loot)
+    end
+    
+    describe "when successful" do
+      before(:each) do
+        @loot.should_receive(:save).and_return(true)
+        get_response
+      end
+      
+      it "should add a flash success message" do
+        flash[:success].should == 'Loot was successfully created.'
+      end
+      
+      it "should redirect to the new loot" do
+        response.should redirect_to(loot_url(@loot))
+      end
+    end
+    
+    describe "when unsuccessful" do
+      it "should render template :new" do
+        @loot.should_receive(:save).and_return(false)
+        get_response
+        response.should render_template(:new)
+      end
+    end
+  end
+  
+  describe "as user" do
+    it "should do nothing" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Update
+# -----------------------------------------------------------------------------
+
+# PUT /loots/:id
+describe LootsController, "#update" do
+  def get_response
+    put :update, :id => '1', :loot => @params
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      find_loot
+      @params = Loot.plan(:raid => mock_model(Raid), :item => mock_model(Item)).stringify_keys!
+    end
+    
+    describe "when successful" do
+      before(:each) do
+        @loot.should_receive(:update_attributes).with(@params).and_return(true)
+        get_response
+      end
+      
+      it "should assign @loot from params" do
+        assigns[:loot].should === @loot
+      end
+      
+      it "should update attributes from params" do
+        # All handled by before
+      end
+      
+      it "should add a flash success message" do
+        flash[:success].should == 'Loot was successfully updated.'
+      end
+      
+      it "should redirect back to the loot" do
+        response.should redirect_to(loot_url('1'))
+      end
+    end
+    
+    describe "when unsuccessful" do
+      it "should render the edit form" do
+        @loot.should_receive(:update_attributes).and_return(false)
+        get_response
+        response.should render_template(:edit)
+      end
+    end
+  end
+  
+  describe "as user" do
+    it "should not render" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Destroy
+# -----------------------------------------------------------------------------
+
+# DELETE /loots/:id
+describe LootsController, "#destroy" do
+  def get_response
+    delete :destroy, :id => '1'
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      find_loot
+      @loot.should_receive(:destroy).and_return(nil)
+      get_response
+    end
+    
+    it "should add a flash success message" do
+      flash[:success].should == 'Loot was successfully deleted.'
+    end
+    
+    it "should redirect to #index" do
+      response.should redirect_to(loots_url)
+    end
+  end
+  
+  describe "as user" do
+    it "should do nothing" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
