136dc6500c48183848718958bb9cf91c9d345568
rspeicher <rspeicher@gmail.com>
2009-03-15 22:52:25 -0400

    Add Items#edit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_filter :find_item, :only => [:show]
+  before_filter :require_admin, :except => [:index, :show]
+  before_filter :find_item, :only => [:show, :edit, :update, :destroy]
   
   def index
     page_title('Items')
@@ -19,6 +20,25 @@ class ItemsController < ApplicationController
     
     respond_to do |wants|
       wants.html
+    end
+  end
+  
+  def edit
+    page_title(@item.name, 'Edit')
+    
+    respond_to do |wants|
+      wants.html
+    end
+  end
+  
+  def update
+    respond_to do |wants|
+      if @item.update_attributes(params[:item])
+        flash[:success] = 'Item was successfully updated.'
+        wants.html { redirect_to(item_path(@item)) }
+      else
+        wants.html { render :action => 'edit' }
+      end
     end
   end
   
--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -27,7 +27,7 @@ end
 # Show
 # -----------------------------------------------------------------------------
 
-# # GET /items/show/:id
+# GET /items/:id
 describe ItemsController, "#show" do
   def get_response
     get :show, :id => '1'
@@ -60,5 +60,115 @@ describe ItemsController, "#show" do
   it "should render" do
     response.should render_template(:show)
     response.should be_success
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Edit
+# -----------------------------------------------------------------------------
+
+# GET /items/:id/edit
+
+describe ItemsController, "#edit" do
+  def get_response
+    get :edit, :id => '1'
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      find_item
+      get_response
+    end
+    
+    it "should assign @item" do
+      assigns[:item].should == @item
+    end
+    
+    it "should render" do
+      response.should render_template(:edit)
+    end
+  end
+  
+  describe "as user" do
+    it "should not render" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+end
+
+# -----------------------------------------------------------------------------
+# Update
+# -----------------------------------------------------------------------------
+
+# PUT /items/:id
+describe ItemsController, "#update" do
+  def get_response
+    put :update, :id => '1', :item => @params
+  end
+  
+  describe "as admin" do
+    before(:each) do
+      login({}, :is_admin? => true)
+      find_item
+      @params = Item.plan.stringify_keys!
+    end
+    
+    describe "when successful" do
+      before(:each) do
+        @item.should_receive(:update_attributes).with(@params).and_return(true)
+        get_response
+      end
+      
+      it "should assign @item from params" do
+        assigns[:item].should === @item
+      end
+      
+      it "should update attributes from params" do
+        # All handled by before
+      end
+      
+      it "should add a flash success message" do
+        flash[:success].should == 'Item was successfully updated.'
+      end
+      
+      it "should redirect back to the item" do
+        response.should redirect_to(item_url('1'))
+      end
+    end
+    
+    describe "when unsuccessful" do
+      it "should render the edit form" do
+        @item.should_receive(:update_attributes).and_return(false)
+        get_response
+        response.should render_template(:edit)
+      end
+    end
+  end
+  
+  describe "as user" do
+    it "should not render" do
+      login({}, :is_admin? => false)
+      get_response
+      response.should redirect_to('/todo')
+    end
+  end
+  
+  describe "as anonymous" do
+    it "should redirect to login" do
+      logout
+      get_response
+      response.should redirect_to(new_user_session_url)
+    end
   end
 end
