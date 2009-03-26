df6f12fa568d1ccc5ecf6a88d6ae9c55998310fb
rspeicher <rspeicher@gmail.com>
2009-03-26 16:39:02 -0400

    Fix link_to_remote_delete fading out the row

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -48,7 +48,7 @@ module ApplicationHelper
     options[:url]     ||= polymorphic_path(object)
     options[:text]    ||= ''
     options[:confirm] ||= 'Are you sure?'
-    options[:success] ||= "$('##{klass}-#{object.id}').fadeOut(250); zebraRows('#{klass}', 300)"
+    options[:success] ||= "$('##{klass}_#{object.id}').fadeOut(250); zebraRows('#{klass}', 300)"
     
     link_to_remote(image_tag('delete.png') + h(options[:text]), 
       :url     => options[:url],
