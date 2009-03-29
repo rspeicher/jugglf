f8668a8523c53bf108d95ff1c8ec321e19c9e864
rspeicher <rspeicher@gmail.com>
2009-03-29 19:17:08 -0400

    Escape the return value of member_completed_achievement(); don't eager load members from completed_achievements

--- app/models/achievement.rb
+++ app/models/achievement.rb
@@ -11,6 +11,6 @@
 #
 
 class Achievement < ActiveRecord::Base
-  has_many :completed_achievements, :include => :member, :dependent => :destroy
+  has_many :completed_achievements, :dependent => :destroy
   has_many :members, :through => :completed_achievements
 end
