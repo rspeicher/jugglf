0f91023c419168892c0f4f822e85f303826f3ee5
rspeicher <rspeicher@gmail.com>
2009-04-03 22:16:50 -0400

    Add rspec and rspec-rails gem dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -32,6 +32,8 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'javan-whenever', :lib => false, :version => '>= 0.1.7', :source => 'http://gems.github.com'
+  config.gem "rspec",          :lib => false, :version => ">= 1.2.0"
+  config.gem "rspec-rails",    :lib => false, :version => ">= 1.2.0"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
