50f2ffa191c314e89d4bbb874043ee5f78bcc8d1
rspeicher <rspeicher@gmail.com>
2009-04-08 16:09:36 -0400

    Make IndexController spec pass by stubbing all of the IndexStat methods.

--- spec/controllers/index_controller_spec.rb
+++ spec/controllers/index_controller_spec.rb
@@ -10,6 +10,18 @@ describe IndexController do
     get :index
   end
   
+  before(:each) do
+    IndexStat.stub!(:attendance_average).and_return(nil)
+    IndexStat.stub!(:loot_factor_average).and_return(nil)
+    IndexStat.stub!(:common_items).and_return(nil)
+    IndexStat.stub!(:common_tokens).and_return(nil)
+    IndexStat.stub!(:most_requested).and_return(nil)
+    IndexStat.stub!(:loots_per_raid).and_return(nil)
+    IndexStat.stub!(:oldest_members).and_return(nil)
+    IndexStat.stub!(:least_recruitable).and_return(nil)
+    IndexStat.stub!(:highest_turnover).and_return(nil)
+  end
+  
   it "should render" do
     get_response
     response.should render_template(:index)
