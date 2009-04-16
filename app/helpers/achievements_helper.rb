0b7af1da18c6508a3d8c557d7d2654f5f06c02ee
rspeicher <rspeicher@gmail.com>
2009-04-16 19:07:58 -0400

    Try to make the Achievement page more width-friendly.

--- app/helpers/achievements_helper.rb
+++ app/helpers/achievements_helper.rb
@@ -1,6 +1,7 @@
 module AchievementsHelper
   def achievement_icon(ach)
-    link_to(image_tag("http://static.wowhead.com/images/icons/small/#{ach.icon}.jpg"), 
+    link_to(image_tag("http://static.wowhead.com/images/icons/small/#{ach.icon}.jpg",
+      :size => "10x10"), 
       "http://www.wowhead.com/?achievement=#{ach.armory_id}")
   end
 end
