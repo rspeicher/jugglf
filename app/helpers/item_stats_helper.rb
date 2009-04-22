0127d63b2b0616bc766306c51409079ef2384eb4
rspeicher <rspeicher@gmail.com>
2009-04-22 19:30:09 -0400

    Attempt to DRY up the ItemStat helper.

--- app/helpers/item_stats_helper.rb
+++ app/helpers/item_stats_helper.rb
@@ -1,25 +1,39 @@
 module ItemStatsHelper
   def link_to_wowhead(item)
     return if item.nil?
-    if item.item_stat.nil?
-      stat = ItemStat.lookup(item.name)
-      item.update_attributes(:item_stat_id => stat.id) unless stat.nil?
-    else
-      stat = item.item_stat
-    end
+    item = stat_lookup(item)
     
-    "<a href='#{stat.wowhead_link}' class='#{stat.color}' target='_new'>#{truncate(h(item.name), :length => 80)}</a>" unless stat.nil?
+    wowhead_link(item)
   end
   
   def link_to_item_with_stats(item)
     return if item.nil?
-    if item.item_stat.nil?
-      stat = ItemStat.lookup(item.name)
-      item.update_attributes(:item_stat_id => stat.id) unless stat.nil?
-    else
-      stat = item.item_stat
+    item = stat_lookup(item)
+    
+    wowhead_link(item, :rel => true)
+  end
+  
+  private
+    def stat_lookup(item)
+      if item.item_stat.nil?
+        stat = ItemStat.lookup(item.name)
+        item.update_attributes(:item_stat_id => stat.id) unless stat.nil?
+      end
+      
+      item
     end
     
-    "<a href='#{item_path(item)}' rel='item=#{stat.wow_id}' class='#{stat.color}'>#{truncate(h(item.name), :length => 80)}</a>" unless stat.nil?
-  end
+    def wowhead_link(item, options = {})
+      return if item.item_stat.nil?
+      
+      options[:rel] ||= false
+      
+      if options[:rel]
+        link_to(truncate(h(item.name), :length => 80), item_path(item), 
+          :class => item.item_stat.color, :rel => "item=#{item.item_stat.wow_id}").gsub(/"/, "'")
+      else
+        link_to(truncate(h(item.name), :length => 80), item.item_stat.wowhead_link, 
+          :class => item.item_stat.color).gsub(/"/, "'")
+      end
+    end
 end
