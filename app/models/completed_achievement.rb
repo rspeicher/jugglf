959f58bbfe9c4aa9f74d6c1bc0e228ef7cc658d2
rspeicher <rspeicher@gmail.com>
2009-04-22 19:00:11 -0400

    Ignore exceptions while fetching achievements.

--- app/models/completed_achievement.rb
+++ app/models/completed_achievement.rb
@@ -17,33 +17,36 @@ class CompletedAchievement < ActiveRecord::Base
     total_achievements = Achievement.count
     
     if total_achievements == 0 or member.completed_achievements.count != total_achievements
-      require 'open-uri'
-      require 'nokogiri'
-      
-      # puts "Processing achievements for #{member.name}"
-      contents = open("http://www.wowarmory.com/character-achievements.xml?r=Mal%27Ganis&n=#{member.name}&c=168",
-        'User-Agent'      => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.7) Gecko/2009021906 Firefox/3.0.7",
-        'Accept-language' => 'enUS'
-      )
-      doc = Nokogiri::XML(contents)
+      begin
+        require 'open-uri'
+        require 'nokogiri'
     
-      # 6th Category = Lich King Heroic Raid
-      # 8th Category = Secrets of Ulduar Heroic
-      doc.search("category:nth(8) achievement").each do |ach|
-        achievement = Achievement.find_or_initialize_by_armory_id(ach['id'])
-      
-        if achievement.new_record?
-          achievement.armory_id   = ach['id']
-          achievement.category_id = ach['categoryId']
-          achievement.title       = ach['title']
-          achievement.icon        = ach['icon']
-          achievement.save
+        # puts "Processing achievements for #{member.name}"
+        contents = open("http://www.wowarmory.com/character-achievements.xml?r=Mal%27Ganis&n=#{member.name}&c=168",
+          'User-Agent'      => "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.5; en-US; rv:1.9.0.7) Gecko/2009021906 Firefox/3.0.7",
+          'Accept-language' => 'enUS'
+        )
+        doc = Nokogiri::XML(contents)
+  
+        # 6th Category = Lich King Heroic Raid
+        # 8th Category = Secrets of Ulduar Heroic
+        doc.search("category:nth(8) achievement").each do |ach|
+          achievement = Achievement.find_or_initialize_by_armory_id(ach['id'])
+    
+          if achievement.new_record?
+            achievement.armory_id   = ach['id']
+            achievement.category_id = ach['categoryId']
+            achievement.title       = ach['title']
+            achievement.icon        = ach['icon']
+            achievement.save
+          end
+    
+          if ach['dateCompleted'] and (not achievement.members.include? member)
+            achievement.completed_achievements.create(:member => member, 
+              :completed_on => ach['dateCompleted'])
+          end
         end
-      
-        if ach['dateCompleted'] and (not achievement.members.include? member)
-          achievement.completed_achievements.create(:member => member, 
-            :completed_on => ach['dateCompleted'])
-        end
+      rescue Exception
       end
     end
   end
