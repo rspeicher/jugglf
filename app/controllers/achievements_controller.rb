eb5192d8f04c855e2d1564b925111e1cfff46750
rspeicher <rspeicher@gmail.com>
2009-06-13 17:56:16 -0400

    Instead of only including certain achievements, we're ignoring redundant ones like Three/Two Lights, Knock and Knock, Knock on Wood, etc.

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -4,11 +4,32 @@ class AchievementsController < ApplicationController
   def index
     page_title('Achievements')
     
-    included = [ 3057, 2929, 2924, 3059, 2944, 2954, 3007, 3184, 3183, 3187, 
-      3189, 3188, 3163 ]
+    ignored = [
+      3010, # Drive Me Crazy
+      2928, # Hot Pocket
+      2965, # I Have the Coolest Friends
+      2974, # I'll Take You All On
+      3011, # Kiss and Make Up
+      3185, # Knock on Wood
+      3186, # Knock, Knock on Wood
+      2936, # Nerf Gravity Bombs
+      2918, # Orbital Bombardment
+      2916, # Orbital Devastation
+      2917, # Nuked from Orbit
+      2978, # Siffed
+      2970, # Staying Buffed All Winter
+      2889, # The Antechamber of Ulduar
+      2893, # The Descent into Madness
+      2891, # The Keepers of Ulduar
+      2895, # The Secrets of Ulduar
+      2887, # The Siege of Ulduar
+      3161, # Three Lights in the Darkness
+      3162, # Two Lights in the Darkness
+      2976, # Who Needs Bloodlust?
+    ]
     
-    @achievements = Achievement.find(:all, :order => 'armory_id', 
-      :conditions => ["category_id = ? AND armory_id IN (#{included.join(',')})", 168])
+    @achievements = Achievement.find(:all, :order => 'title', 
+      :conditions => ["category_id = ? AND armory_id NOT IN (#{ignored.join(',')})", 168])
     @members = Member.active.find(:all, :include => :achievements)
     
     respond_to do |wants|
