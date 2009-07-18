073f6ed2ad665da2a4a5c34a70aac5a9cf01f78b
rspeicher <rspeicher@gmail.com>
2009-07-17 21:59:23 -0400

    Don't validate_uniqueness_of :name anymore since it's a Name-ID uniqueness, not just Name.

--- app/models/item.rb
+++ app/models/item.rb
@@ -29,7 +29,6 @@ class Item < ActiveRecord::Base
   
   # Validations ---------------------------------------------------------------
   validates_presence_of :name
-  validates_uniqueness_of :name
   
   # Callbacks -----------------------------------------------------------------
   
