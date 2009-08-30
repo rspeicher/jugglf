3e255599f2e87c529697d91ae28f2d1faaa5afa8
rspeicher <rspeicher@gmail.com>
2009-08-30 16:16:05 -0400

    Change the 'staging' deployment to use the 'staging' branch.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,7 +1,7 @@
 set :domain,      "staging.tsigo.org"
 set :repository,  "git@gluttony:#{application}.git"
 set :scm,         'git'
-set :branch,      'master'
+set :branch,      'staging'
 
 set :deploy_to,   "/home/tsigo/rails/#{application}"
 
