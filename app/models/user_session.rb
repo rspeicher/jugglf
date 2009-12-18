018bfb594fcd05e9e31f540451ae9fdd0cfc2e99
rspeicher <rspeicher@gmail.com>
2009-12-18 10:36:51 -0500

    Change authenticate_with call to InvisionBridge

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,5 +1,5 @@
 class UserSession < Authlogic::Session::Base
-  authenticate_with InvisionUser
+  authenticate_with InvisionBridge::InvisionUser
   last_request_at_threshold 1.minute
   params_key 'api_key'
 end
