bebb578f1e286736bdcbea7d89a15615bc049f20
rspeicher <rspeicher@gmail.com>
2009-12-21 18:34:47 -0500

    Make Member.user work

--- app/models/member.rb
+++ app/models/member.rb
@@ -43,7 +43,7 @@ class Member < ActiveRecord::Base
   
   belongs_to :rank, :class_name => "MemberRank", :foreign_key => "rank_id"
 
-  belongs_to :user, :class_name => "InvisionUser", :foreign_key => "user_id"  
+  belongs_to :user, :class_name => "InvisionBridge::InvisionUser", :foreign_key => "user_id"
 
   has_many :wishlists, :include => :item, :order => 'priority', :dependent => :destroy
   
