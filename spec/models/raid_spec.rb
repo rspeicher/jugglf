b5cb7f8dc6f398eb2f2eb79b56f665d0ef360640
rspeicher <rspeicher@gmail.com>
2009-12-30 17:09:46 -0500

    Fix weird spec failure.

--- spec/models/raid_spec.rb
+++ spec/models/raid_spec.rb
@@ -207,6 +207,7 @@ end
 
 describe Raid, "#parse_drops" do
   before(:all) do
+    FakeWeb.clean_registry
     FakeWeb.register_uri(:get, %r(http://www.wowarmory.com/search\.xml\?.+), :body => File.read(File.dirname(__FILE__) + "/../fixtures/wowarmory/search_torch_of_holy_fire.xml"))
     FakeWeb.register_uri(:get, "http://www.wowarmory.com/item-tooltip.xml?i=40395", :body => File.read(File.dirname(__FILE__) + "/../fixtures/wowarmory/item-tooltip_40395.xml"))
   end
