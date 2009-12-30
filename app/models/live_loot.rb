9f890d99263372eb6afc79c59be32cc278a14c54
rspeicher <rspeicher@gmail.com>
2009-12-30 16:14:45 -0500

    Implement LiveLoot.from_text method.
    This will eventually be the method we use to take input from a textarea and create LiveLoots.

--- app/models/live_loot.rb
+++ app/models/live_loot.rb
@@ -13,4 +13,47 @@ class LiveLoot < ActiveRecord::Base
   attr_accessible :wow_id, :item_name, :member_name, :loot_type
   validates_presence_of :item_name
   validates_inclusion_of :loot_type, :in => %w(bis rot sit bisrot), :allow_nil => true
+  
+  # Takes a block of text in a specific format and creates instances of +LiveLoot+ objects.
+  #
+  # For example, passing in the following text...
+  #
+  #   DE, Tsigo - [Snowserpent Mail Helm]|49952
+  #   Duskshadow - [Crushing Coldwraith Belt]|49978
+  #   Sebudai (sit) - Stygian Bladebreaker|47255
+  #   Kazanir bis - [Death's Choice]|47303
+  #
+  # ...will return an array with the following *unsaved* objects
+  #
+  #   #<LiveLoot id: nil, wow_id: 49952, item_name: "Snowserpent Mail Helm", member_name: nil, loot_type: nil>
+  #   #<LiveLoot id: nil, wow_id: 49952, item_name: "Snowserpent Mail Helm", member_name: "Tsigo", loot_type: nil>
+  #   #<LiveLoot id: nil, wow_id: 49978, item_name: "Crushing Coldwraith Belt", member_name: "Duskshadow", loot_type: nil>
+  #   #<LiveLoot id: nil, wow_id: 47255, item_name: "Stygian Bladebreaker", member_name: "Sebudai", loot_type: "sit">
+  #   #<LiveLoot id: nil, wow_id: 47303, item_name: "Death's Choice", member_name: "Kazanir", loot_type: "bis">
+  #
+  # Note that both the parenthesis around loot types and the brackets around the item name are optional.
+  def self.from_text(input)
+    retval = []
+    return retval unless input.present?
+    
+    input.split("\n").each do |line|
+      matches = line.match(/^([\w,\(\)\s]+) - ([^\|]+)(?:\|([0-9]+))?$/)
+      if matches.length >= 2
+        buyers = matches[1].split(',')
+        buyers.each do |buyer|
+          buyer.strip!
+          
+          loot = LiveLoot.new
+          loot.wow_id      = matches[3].to_i
+          loot.item_name   = matches[2].gsub(/\[(.+)\]/, '\1').strip # Remove brackets
+          loot.member_name = buyer.gsub(/^([^\s]+).*/, '\1') unless buyer == 'DE'
+          loot.loot_type   = buyer.gsub(/^(\w+) \(?(bis|rot|bisrot|sit)\)?$/, '\2') if buyer.match(/\s\(?(bis|rot|bisrot|sit)\)?$/)
+          
+          retval << loot
+        end
+      end
+    end
+  
+    retval
+  end
 end
--- spec/models/live_loot_spec.rb
+++ spec/models/live_loot_spec.rb
@@ -35,3 +35,49 @@ describe LiveLoot do
   it { should allow_mass_assignment_of(:member_name) }
   it { should allow_mass_assignment_of(:loot_type) }
 end
+
+describe LiveLoot, ".from_text" do
+  before(:all) do
+    @text = "DE, Tsigo - [Snowserpent Mail Helm]|49952\n" +
+      "Duskshadow - [Crushing Coldwraith Belt]|49978\n" +
+      "Sebudai (sit) - Stygian Bladebreaker|47255\n" +
+      "Kazanir bis - [Death's Choice]|47303\n"
+  end
+  
+  it "should return an empty array if no text is given" do
+    LiveLoot.from_text(nil).should eql([])
+  end
+  
+  describe "parsing" do
+    before(:all) do
+      @expected = [
+        { :wow_id => 49952, :item_name => "Snowserpent Mail Helm", :member_name => nil, :loot_type => nil },
+        { :wow_id => 49952, :item_name => "Snowserpent Mail Helm", :member_name => "Tsigo", :loot_type => nil },
+        { :wow_id => 49978, :item_name => "Crushing Coldwraith Belt", :member_name => "Duskshadow", :loot_type => nil },
+        { :wow_id => 47255, :item_name => "Stygian Bladebreaker", :member_name => "Sebudai", :loot_type => 'sit' },
+        { :wow_id => 47303, :item_name => "Death's Choice", :member_name => "Kazanir", :loot_type => 'bis' },
+      ]
+      
+      @loots = LiveLoot.from_text(@text)
+    end
+    
+    it "should return an array of LiveLoot objects" do
+      @loots.length.should eql(5)
+      @loots[0].should be_a(LiveLoot)
+    end
+    
+    it "should not save the LiveLoot record" do
+      @loots[0].new_record?.should be_true
+    end
+    
+    0.upto(4) do |i|
+      describe "Loot #{i}" do
+        %w(wow_id item_name member_name loot_type).each do |param|
+          it "should correctly set the #{param} attribute" do
+            @loots[i].send(param).should eql(@expected[i][param.intern])
+          end
+        end
+      end
+    end
+  end
+end
