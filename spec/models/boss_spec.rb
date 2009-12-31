e0900b6230deb5fa2793984b324e972af1d9a6d1
rspeicher <rspeicher@gmail.com>
2009-12-31 17:09:04 -0500

    Add a minor validation to Boss.

--- spec/models/boss_spec.rb
+++ spec/models/boss_spec.rb
@@ -10,4 +10,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Boss do
+  before(:each) do
+    @boss = Boss.make
+  end
+  
+  it "should be valid" do
+    @boss.should be_valid
+  end
+  
+  it { should validate_presence_of(:name) }
 end
