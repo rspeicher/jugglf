5799ec334e257a196253027af89474845f152068
rspeicher <rspeicher@gmail.com>
2009-12-31 18:37:46 -0500

    Forgot to rename Attendance::LootsControllerSpec.

---
+++ spec/controllers/attendance/loots_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe Attendance::LootsController do
+
+  #Delete this example and add some real ones
+  it "should use Attendance::LootsController" do
+    controller.should be_an_instance_of(Attendance::LootsController)
+  end
+
+end
