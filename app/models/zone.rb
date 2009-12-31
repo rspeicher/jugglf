522e8d8d964ae82b6a851393ec9d94940f88b61b
rspeicher <rspeicher@gmail.com>
2009-12-31 18:11:33 -0500

    Minor Zone validations.

--- app/models/zone.rb
+++ app/models/zone.rb
@@ -9,4 +9,6 @@
 
 class Zone < ActiveRecord::Base
   has_many :loot_tables, :as => :object, :dependent => :destroy
+  
+  validates_presence_of :name
 end
--- spec/models/zone_spec.rb
+++ spec/models/zone_spec.rb
@@ -10,7 +10,13 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Zone do
-  it "should be valid" do
-    Zone.make.should be_valid
+  before(:each) do
+    @zone = Zone.make
   end
+  
+  it "should be valid" do
+    @zone.should be_valid
+  end
+  
+  it { should validate_presence_of(:name) }
 end
