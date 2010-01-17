3119a27cfe3fa83e324501b0e881cd85cfd5128b
rspeicher <rspeicher@gmail.com>
2010-01-17 16:32:41 -0500

    Change Wishlist lua format; add a helper to group notes by item ID.

--- app/helpers/members/wishlists_helper.rb
+++ app/helpers/members/wishlists_helper.rb
@@ -1,2 +1,13 @@
 module Members::WishlistsHelper
+  def group_wishlist_notes(wishlists)
+    retval = {}
+    return retval if wishlists.blank?
+    
+    wishlists.each do |wishlist|
+      retval[wishlist.wow_id] ||= []
+      retval[wishlist.wow_id] << {:priority => wishlist.priority, :note => wishlist.note}
+    end
+    
+    retval
+  end
 end
