4d64bbb8289b8625e8123986dc7008d1e397a56e
rspeicher <rspeicher@gmail.com>
2010-01-21 18:07:54 -0500

    Juggy was still using wow_id; specs still broken

--- vendor/plugins/juggy/lib/juggy.rb
+++ vendor/plugins/juggy/lib/juggy.rb
@@ -4,84 +4,84 @@ module Juggy
   class << self
     def parse_attendees(output)
       return unless output.present?
-      
+
       retval = []
- 
+
       require 'csv'
       lines = CSV.parse(output) do |line|
         next if line[0].nil? or line[0].strip.empty?
         next if line[1].nil? or line[1].strip.empty?
-        
+
         retval << { :name => line[0].strip, :attendance => line[1].to_f }
       end
-      
+
       retval
     end
-    
+
     def parse_loots(output)
       return if output.nil? or output.empty?
-    
+
       retval = []
-    
+
       lines = output.split("\n")
       lines.each do |line|
         split = line.split(' - ')
         next unless split.length == 2
-      
+
         buyers    = split[0].split(',')
         item_name = split[1].gsub(/\[(.+)\]/, '\1').strip # Item name, no brackets
-      
+
         buyers.each do |buyer|
           loot = parse_loot_line(buyer, item_name)
-        
+
           retval << loot
         end
       end
-    
+
       retval
     end
-    
+
     private
       def parse_loot_line(buyer, item_name)
         return unless buyer.present? and item_name.present?
         buyer.strip!
         item_name.strip!
-        
+
         retval = { :item   => nil, :member => nil }
-        
+
         # Now an Item ID might be included in the form "Name|ID", so see if we got one
-        item_name, wow_id = item_name.split('|')
-        
+        item_name, item_id = item_name.split('|')
+
         # Fetch an item record based on its name or ID
-        if wow_id.nil?
+        if item_id.nil?
           retval[:item] = Item.find_or_initialize_by_name(item_name)
         else
-          retval[:item] = Item.find_or_initialize_by_wow_id(wow_id)
+          retval[:item] = Item.find_or_initialize_by_id(item_id)
         end
-        
+
         # Set BiS/Rot/Sit
         set_loot_types(retval, buyer)
-      
+
         # Only set the buyer if it's not disenchanted
         unless buyer == 'DE'
           retval[:member] = Member.find_or_initialize_by_name(buyer.gsub(/^(\w+).*?$/, '\1'))
         end
-      
+
         # Item Pricing
         retval[:price] = price(retval[:item], retval[:member])
 
         retval
       end
-      
+
       def set_loot_types(loot, buyer)
         # These next regex just mean "contained within parenthesis where the only
-        # other values are a-z and \s"; Prevents "Tsitgo" as a name from 
+        # other values are a-z and \s"; Prevents "Tsitgo" as a name from
         # matching "sit" as a tell type while still allowing "(bis rot)"
         loot[:best_in_slot] = !buyer.match(/\(([a-z\s]+)?bis([a-z\s]+)?\)/).nil?
         loot[:situational]  = !buyer.match(/\(([a-z\s]+)?sit([a-z\s]+)?\)/).nil?
         loot[:rot]          = !buyer.match(/\(([a-z\s]+)?rot([a-z\s]+)?\)/).nil?
       end
-      
+
       # Given an initialized Item object
       def price(item, member = nil)
         return if member.nil?
@@ -89,12 +89,12 @@ module Juggy
         price = nil
 
         item.lookup # Get the data we need if we haven't already
-        unless item.wow_id.nil?
+        unless item.id.nil?
           price = ItemPrice.instance.price(:name => item.name,
             :level => item.level, :slot => item.slot,
-            :class => member.wow_class, :id => item.wow_id)
+            :class => member.wow_class, :id => item.id)
         end
-        
+
         price
       end
   end
--- vendor/plugins/juggy/spec/spec/juggy_spec.rb
+++ vendor/plugins/juggy/spec/spec/juggy_spec.rb
@@ -2,89 +2,86 @@ require 'spec_helper'
 
 describe Juggy do
   before(:each) do
-    Item.destroy_all
-    
     @attendance_output = "Sebudai,1.00,233"
     @loot_output = "Sebudai - [Arachnoid Gold Band]"
   end
-  
+
   describe "#parse_loots" do
     it "should return an array of hashes" do
       loots = Juggy.parse_loots(@loot_output)
-      
+
       loots.size.should eql(1)
       loots[0].class.should eql(Hash)
     end
-    
+
     it "should not save loots automatically" do
       lambda { Juggy.parse_loots(@loot_output) }.
         should_not change(Loot, :count)
     end
-    
+
     it "should figure out price based on item stats" do
       # Juggy.parse_loots tries to figure out the price via Item's
       # data; fake that so we don't hit Wowhead for non-existant items
-      Item.stub!(:find_or_initialize_by_name).and_return(Item.make)
-      
+      Item.stub!(:find_or_initialize_by_name).and_return(Factory(:item))
+
       loots = Juggy.parse_loots('Buyer - Item')
       loots[0][:price].should eql(3.00) # Price for a 258 Head slot
     end
-    
+
     describe "multiple items of the same name" do
       it "should create a non-existent item if given the ID" do
         Item.destroy_all
         old_item = Item.make(:old_onyxia_head)
         new_item = Item.make_unsaved(:new_onyxia_head)
-        loots = Juggy.parse_loots("Tsigo - [#{new_item.name}]|#{new_item.wow_id}")
+        loots = Juggy.parse_loots("Tsigo - [#{new_item.name}]|#{new_item.id}")
         loots[0][:item].inspect.should eql(new_item.inspect)
       end
     end
-    
+
     describe "loot details" do
       before(:each) do
-        @item = Item.make(:with_real_stats)
+        @item = Factory(:item_with_real_stats)
       end
-    
+
       it "should return one Hash" do
         loot = Juggy.parse_loots("Sebudai - #{@item.name}")
         loot[0].class.should eql(Hash)
       end
-    
+
       it "should correctly set best_in_slot" do
         loot = Juggy.parse_loots("Modrack (bis) - #{@item.name}")
         loot[0][:best_in_slot].should be_true
       end
-    
+
       it "should correctly set situational" do
         loot = Juggy.parse_loots("Modrack (sit) - #{@item.name}")
         loot[0][:situational].should be_true
       end
-    
+
       it "should correctly set rot" do
         loot = Juggy.parse_loots("Modrack (rot) - #{@item.name}")
         loot[0][:rot].should be_true
       end
-    
+
       it "should correctly set best_in_slot and rot at the same time" do
         loot = Juggy.parse_loots("Modrack (bis rot) - #{@item.name}")
         loot[0][:best_in_slot].should be_true
         loot[0][:rot].should be_true
       end
-    
+
       it "should not have false positives for purchase types inside buyer names" do
         loot = Juggy.parse_loots("Sebisudai - #{@item.name}")
         loot[0][:best_in_slot].should be_false
       end
-    
+
       it "should set member as nil if buyer is 'DE'" do
         loot = Juggy.parse_loots("DE - #{@item.name}")
         loot[0][:member].should be_nil
       end
-      
-      it "should use the wow_id if provided" do
-        Item.destroy_all
-        glaive_main = Item.make(:name => 'Warglaive of Azzinoth', :wow_id => 32837)
-        glaive_off  = Item.make(:name => 'Warglaive of Azzinoth', :wow_id => 32838)
+
+      it "should use the id if provided" do
+        glaive_main = Item.make(:name => 'Warglaive of Azzinoth', :id => 32837)
+        glaive_off  = Item.make(:name => 'Warglaive of Azzinoth', :id => 32838)
         loot = Juggy.parse_loots("Kamien (bis) - [Ignore Me]|32838")
         loot[0][:item].should eql(glaive_off)
       end
@@ -94,22 +91,22 @@ describe Juggy do
       #   @loots[:no_parens][:best_in_slot].should be_true
       #   @loots[:no_parens][:member].name.should eql('Kamien')
       # end
-      
+
       describe "for multiple buyers" do
         before(:each) do
           @loot = Juggy.parse_loots("Modrack (bis), Rosoo (sit), DE - #{@item.name}")
         end
-        
+
         it "should get the correct number of buyers" do
           @loot.size.should eql(3)
         end
-        
+
         it "should get the correct buyer names" do
           @loot[0][:member].name.should eql('Modrack')
           @loot[1][:member].name.should eql('Rosoo')
           @loot[2][:member].should be_nil
         end
-        
+
         it "should get the correct item types" do
           @loot[0][:best_in_slot].should be_true
           @loot[1][:situational].should be_true
@@ -117,22 +114,22 @@ describe Juggy do
       end
     end
   end
-  
+
   describe "#parse_attendees" do
     before(:each) do
       @att = Juggy.parse_attendees(@attendance_output)
     end
-    
+
     it "should return an array of hashes" do
       @att.size.should eql(1)
       @att[0].class.should eql(Hash)
     end
-    
+
     it "should return duplicate members when provided" do
       output = ''
       3.times { output += @attendance_output + "\n" }
       @att = Juggy.parse_attendees(output)
-      
+
       @att.size.should eql(3)
     end
   end
