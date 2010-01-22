ae364f719e6eae142f0d2a60ad5ff7837b04809f
rspeicher <rspeicher@gmail.com>
2010-01-22 18:25:34 -0500

    Delete searchify

--- vendor/plugins/searchify/generators/searchify_javascript/searchify_javascript_generator.rb
+++
@@ -1,7 +0,0 @@
-class SearchifyJavascriptGenerator < Rails::Generator::Base
-  def manifest
-    record do |m|
-      m.file "searchify.js", "public/javascripts/searchify.js"
-    end
-  end
-end
--- vendor/plugins/searchify/lib/searchify.rb
+++
@@ -1,9 +0,0 @@
-$:.unshift(File.dirname(__FILE__))
-require 'searchify/searcher'
-require 'searchify/facet'
-require 'searchify/parent_facet'
-require 'searchify/model_additions'
-require 'searchify/conditions_merger'
-require 'searchify/facets_builder'
-require 'searchify/search_options'
-require 'searchify/view_helper'
