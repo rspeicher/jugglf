35dbfb54ed2b7e16b2644aa3e39de82be7de8a51
rspeicher <rspeicher@gmail.com>
2010-09-03 03:01:40 -0400

    Rails 3 - Fix User.juggernaut scope

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < InvisionBridge::User::Base
   MEMBER_GROUP    = 8
   APPLICANT_GROUP = 9
 
-  scope :juggernaut, order(:name), where(:member_group_id => [ADMIN_GROUP, MEMBER_GROUP, APPLICANT_GROUP])
+  scope :juggernaut, order(:name).where(:member_group_id => [ADMIN_GROUP, MEMBER_GROUP, APPLICANT_GROUP])
   has_one :member, :foreign_key => "user_id"
 
   def is_admin?
